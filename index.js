b5c257f4e4b9ef896023ed635e8e87180f5ef80c
Berkay Beyaz <beyazberkay407@gmail.com>
2016-12-23 21:51:26 +0200

    Changed project structure and added constructor method for npm

--- index.js
+++ index.js
@@ -2,296 +2,308 @@ let fetch = require('node-fetch');
 
 // TODO: DEBUG ENV CONTROL
 const API_LOCATION = 'https://api.hurriyet.com.tr/v1';
-const TOKEN = '';
-const headers = { 
+let headers = { 
 	'Content-Types': 'application/json',
-	'apikey': TOKEN,
+	'apikey': null,
 };
 
-function sendRequest({ endpoint, parameters }) {
-	return new Promise((resolve,reject) => {
-		const endPoint = API_LOCATION + '/'+endpoint+'/'+getQueryString(parameters)
-		fetch(endPoint, {
-			method: 'GET',
-			headers,
-		}).then((response) => {
-			return response.json();
-		}).then((responseJson) => {
-			console.log(responseJson);
-			 resolve(responseJson);
-			 return;
+class HurriyetAPIWrapper {
+	constructor({ token, }) {
+		if (token) {
+			headers.apikey = token;
+		} else {
+			console.log('You must supply a API key.');
+			header.apikey = '';
+		}
+	}
+
+	sendRequest({ endpoint, parameters }) {
+		return new Promise((resolve,reject) => {
+			const endPoint = API_LOCATION + '/'+endpoint+'/'+getQueryString(parameters)
+			fetch(endPoint, {
+				method: 'GET',
+				headers,
+			}).then((response) => {
+				return response.json();
+			}).then((responseJson) => {
+				console.log(responseJson);
+				resolve(responseJson);
+				return;
+			})
 		})
-	})
+	}
+
+	getQueryString(parameters){
+		if(!parameters || parameters.constructor !== Array){
+			console.log('Parameters should be array.');
+			return '';
+		}
+		let queryString = ''
+		for(var i = 0 ; i < parameters.length ; i++){
+			if(i === 0)
+				queryString += '';
+			else
+				queryString += '&';
+			const objectKey = Object.keys(parameters[i]);
+			queryString += parameters[i][objectKey];
+		}
+
+		return queryString;
+	}
+
+	getArticles(args = {}) {
+		let {
+			articleId,
+			filter,
+			select,
+			top,
+		} = args;
+
+		let parameters = [];
+
+		if (filter) {
+			parameters.push({ filter });
+		}
+
+		if (articleId) {
+			if (select) {
+				parameters.push({ select });
+			}
+
+			if (top) {
+				parameters.push({ top });
+			}
+
+			sendRequest({ 
+				endpoint: 'articles/' + articleId,
+				parameters: parameters
+			});
+		} else {
+			sendRequest({ 
+				endpoint: 'articles',
+				parameters: parameters
+			});
+		}
+	}
+
+	getPaths(args = {}) {
+		let {
+			pathId,
+			filter,
+			select,
+			top,
+		} = args;
+
+		let parameters = [];
+
+		if (filter) {
+			parameters.push({ filter });
+		}
+
+		if (pathId) {
+			if (select) {
+				parameters.push({ select });
+			}
+
+			if (top) {
+				parameters.push({ top });
+			}
+
+			sendRequest({ 
+				endpoint: 'paths/' + pathId,
+				parameters: parameters
+			});
+		} else {
+			sendRequest({ 
+				endpoint: 'paths',
+				parameters: parameters
+			});
+		}
+	}
+
+	getColumns(args = {}) {
+		let {
+			columnId,
+			filter,
+			select,
+			top,
+		} = args;
+
+		let parameters = [];
+
+		if (filter) {
+			parameters.push({ filter });
+		}
+
+		if (columnId) {
+			if (select) {
+				parameters.push({ select });
+			}
+
+			if (top) {
+				parameters.push({ top });
+			}
+
+			sendRequest({ 
+				endpoint: 'columns/' + columnId,
+				parameters: parameters
+			});
+		} else {
+			sendRequest({ 
+				endpoint: 'columns',
+				parameters: parameters
+			});
+		}
+	}
+
+	getDates(args = {}) {
+		let {
+		} = args;
+
+		let parameters = [];
+
+		sendRequest({ 
+			endpoint: 'date',
+			parameters: parameters
+		});
+	}
+
+	getNewsPhotoGalleries(args = {}) {
+		let {
+			galleryId,
+			filter,
+			select,
+			top,
+		} = args;
+
+		let parameters = [];
+
+		if (filter) {
+			parameters.push({ filter });
+		}
+
+		if (galleryId) {
+			if (select) {
+				parameters.push({ select });
+			}
+
+			if (top) {
+				parameters.push({ top });
+			}
+
+			sendRequest({ 
+				endpoint: 'newsphotogalleries/' + galleryId,
+				parameters: parameters
+			});
+		} else {
+			sendRequest({ 
+				endpoint: 'newsphotogalleries',
+				parameters: parameters
+			});
+		}
+	}
+
+	getPages(args = {}) {
+		let {
+			pageId,
+			filter,
+			select,
+			top,
+		} = args;
+
+		let parameters = [];
+
+		if (filter) {
+			parameters.push({ filter });
+		}
+
+		if (pageId) {
+			if (select) {
+				parameters.push({ select });
+			}
+
+			if (top) {
+				parameters.push({ top });
+			}
+
+			sendRequest({ 
+				endpoint: 'pages/' + pageId,
+				parameters: parameters
+			});
+		} else {
+			sendRequest({ 
+				endpoint: 'pages',
+				parameters: parameters
+			});
+		}
+	}
+
+	getWriters(args = {}) {
+		let {
+			writerId,
+			filter,
+			select,
+			top,
+		} = args;
+
+		let parameters = [];
+
+		if (filter) {
+			parameters.push({ filter });
+		}
+
+		if (writerId) {
+			if (select) {
+				parameters.push({ select });
+			}
+
+			if (top) {
+				parameters.push({ top });
+			}
+
+			sendRequest({ 
+				endpoint: 'writers/' + writerId,
+				parameters: parameters
+			});
+		} else {
+			sendRequest({ 
+				endpoint: 'writers',
+				parameters: parameters
+			});
+		}
+	}
+
+	search(args = {}) {
+		let {
+			skip,
+			top,
+			sort,
+			keyword,
+		} = args;
+
+		let parameters = [];
+
+		if (keyword) {
+			if (skip) {
+				parameters.push({ skip });
+			}
+
+			if (top) {
+				parameters.push({ top });
+			}
+
+			if (sort) {
+				parameters.push({ sort });
+			}
+
+			sendRequest({ 
+				endpoint: 'search/' + keyword,
+				parameters: parameters
+			});
+		} else {
+			console.log('You have to support a keyword to this API call.');
+		}
+	}
 }
 
-function getQueryString(parameters){
-	if(!parameters || parameters.constructor !== Array){
-		console.log('Parameters should be array.');
-		return '';
-	}
-	let queryString = ''
-	for(var i = 0 ; i < parameters.length ; i++){
-		if(i === 0)
-			queryString += '';
-		else
-			queryString += '&';
-		const objectKey = Object.keys(parameters[i]);
-		queryString += parameters[i][objectKey];
-	}
-
-	return queryString;
-}
-
-function getArticles(args = {}) {
-	let {
-		articleId,
-		filter,
-		select,
-		top,
-	} = args;
-
-	let parameters = [];
-
-	if (filter) {
-		parameters.push({ filter });
-	}
-
-	if (articleId) {
-		if (select) {
-			parameters.push({ select });
-		}
-
-		if (top) {
-			parameters.push({ top });
-		}
-
-		sendRequest({ 
-			endpoint: 'articles/' + articleId,
-			parameters: parameters
-		});
-	} else {
-		sendRequest({ 
-			endpoint: 'articles',
-			parameters: parameters
-		});
-	}
-}
-
-function getPaths(args = {}) {
-	let {
-		pathId,
-		filter,
-		select,
-		top,
-	} = args;
-
-	let parameters = [];
-
-	if (filter) {
-		parameters.push({ filter });
-	}
-
-	if (pathId) {
-		if (select) {
-			parameters.push({ select });
-		}
-
-		if (top) {
-			parameters.push({ top });
-		}
-
-		sendRequest({ 
-			endpoint: 'paths/' + pathId,
-			parameters: parameters
-		});
-	} else {
-		sendRequest({ 
-			endpoint: 'paths',
-			parameters: parameters
-		});
-	}
-}
-
-function getColumns(args = {}) {
-	let {
-		columnId,
-		filter,
-		select,
-		top,
-	} = args;
-
-	let parameters = [];
-
-	if (filter) {
-		parameters.push({ filter });
-	}
-
-	if (columnId) {
-		if (select) {
-			parameters.push({ select });
-		}
-
-		if (top) {
-			parameters.push({ top });
-		}
-
-		sendRequest({ 
-			endpoint: 'columns/' + columnId,
-			parameters: parameters
-		});
-	} else {
-		sendRequest({ 
-			endpoint: 'columns',
-			parameters: parameters
-		});
-	}
-}
-
-function getDates(args = {}) {
-	let {
-	} = args;
-
-	let parameters = [];
-
-	sendRequest({ 
-		endpoint: 'date',
-		parameters: parameters
-	});
-}
-
-function getNewsPhotoGalleries(args = {}) {
-	let {
-		galleryId,
-		filter,
-		select,
-		top,
-	} = args;
-
-	let parameters = [];
-
-	if (filter) {
-		parameters.push({ filter });
-	}
-
-	if (galleryId) {
-		if (select) {
-			parameters.push({ select });
-		}
-
-		if (top) {
-			parameters.push({ top });
-		}
-
-		sendRequest({ 
-			endpoint: 'newsphotogalleries/' + galleryId,
-			parameters: parameters
-		});
-	} else {
-		sendRequest({ 
-			endpoint: 'newsphotogalleries',
-			parameters: parameters
-		});
-	}
-}
-
-function getPages(args = {}) {
-	let {
-		pageId,
-		filter,
-		select,
-		top,
-	} = args;
-
-	let parameters = [];
-
-	if (filter) {
-		parameters.push({ filter });
-	}
-
-	if (pageId) {
-		if (select) {
-			parameters.push({ select });
-		}
-
-		if (top) {
-			parameters.push({ top });
-		}
-
-		sendRequest({ 
-			endpoint: 'pages/' + pageId,
-			parameters: parameters
-		});
-	} else {
-		sendRequest({ 
-			endpoint: 'pages',
-			parameters: parameters
-		});
-	}
-}
-
-function getWriters(args = {}) {
-	let {
-		writerId,
-		filter,
-		select,
-		top,
-	} = args;
-
-	let parameters = [];
-
-	if (filter) {
-		parameters.push({ filter });
-	}
-
-	if (writerId) {
-		if (select) {
-			parameters.push({ select });
-		}
-
-		if (top) {
-			parameters.push({ top });
-		}
-
-		sendRequest({ 
-			endpoint: 'writers/' + writerId,
-			parameters: parameters
-		});
-	} else {
-		sendRequest({ 
-			endpoint: 'writers',
-			parameters: parameters
-		});
-	}
-}
-
-function search(args = {}) {
-	let {
-		skip,
-		top,
-		sort,
-		keyword,
-	} = args;
-
-	let parameters = [];
-
-	if (keyword) {
-		if (skip) {
-			parameters.push({ skip });
-		}
-
-		if (top) {
-			parameters.push({ top });
-		}
-
-		if (sort) {
-			parameters.push({ sort });
-		}
-
-		sendRequest({ 
-			endpoint: 'search/' + keyword,
-			parameters: parameters
-		});
-	} else {
-		console.log('You have to support a keyword to this API call.');
-	}
-}
+module.exports = HurriyetAPIWrapper;
